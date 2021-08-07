a903d7c5f6e1612447143f4dadc80d0f234f8c9c
0x6de <0xyde@protonmail.com>
2021-08-07 19:25:42 +0200

    Check if twitch parameter is defined

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const config = require('./config');
 const clientSteam = new SteamUser();
 
 var myArgs = process.argv.slice(2);
-console.log('Twitch channel parameter: ', myArgs);
+console.log('Twitch channel parameter: ', myArgs[0]);
 const SteamKey = new RegExp('((?![^0-9]{12,}|[^A-z]{12,})([A-z0-9]{4,5}-?[A-z0-9]{4,5}-?[A-z0-9]{4,5}(-?[A-z0-9]{4,5}(-?[A-z0-9]{4,5})?)?))');
 
 const logInOptions = {
@@ -15,39 +15,45 @@ const logInOptions = {
     autoRelogin: true
 };
 
-const client = new tmi.Client({
-	channels: [ myArgs[0] ],
-    connection: {reconnect: true}
-});
+if (myArgs[0] === undefined) {
+    console.log("You need to type a twitch channel name in parameter \nexample: node app.js your_twitch_channel_name");
+    process.exit();
+} else {
+    const client = new tmi.Client({
+        channels: [ myArgs[0] ],
+        connection: {reconnect: true}
+    });
+    clientSteam.logOn(logInOptions);
 
-clientSteam.logOn(logInOptions);
+    clientSteam.on('loggedOn', () => {
+        console.log('Steam connected');
+        client.connect();
+    });
 
-clientSteam.on('loggedOn', () => {
-	console.log('Steam connected');
-    client.connect();
-});
+    client.on('connected', () => {
+        console.log("Twitch chat connected");
+    })
 
-client.on('connected', () => {
-    console.log("Twitch chat connected");
-})
+    client.on('message', (channel, tags, message, self) => {
+        console.log(`${tags['display-name']}: ${message}`);
+        if (SteamKey.test(message)) {
+            var key = message.match(SteamKey)
+            console.log("STEAM KEY DETECTED: " + message + "\nKey: " + key[0])
+            clientSteam.redeemKey(key[0], function (err) {
+                if (err) {
+                    console.log("INVALID KEY OR ALREADY REDEEMED")
+                }else {
+                    console.log("KEY ACTIVATED");
+                    time = new Date();
+                    fs.appendFile('logs.txt', '\nKey activated with success: ' + key[0] + ' || ' + time, function (err) {
+                        if (err) throw err;
+                        console.log('logs.txt updated');
+                    });
+                }
+            })
+        }
+    });		
+}
 
-client.on('message', (channel, tags, message, self) => {
-	console.log(`${tags['display-name']}: ${message}`);
-    if (SteamKey.test(message)) {
-        var key = message.match(SteamKey)
-        console.log("STEAM KEY DETECTED: " + message + "\nKey: " + key[0])
-        clientSteam.redeemKey(key[0], function (err) {
-            if (err) {
-                console.log("INVALID KEY OR ALREADY REDEEMED")
-            }else {
-                console.log("KEY ACTIVATED");
-                time = new Date();
-                fs.appendFile('logs.txt', '\nKey activated with success: ' + key[0] + ' || ' + time, function (err) {
-                    if (err) throw err;
-                    console.log('logs.txt updated');
-                 });
-            }
-        })
-    }
-});
-		
+
+
